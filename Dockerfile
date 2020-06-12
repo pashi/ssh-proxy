4351a6fc3206b27ac063e961b8f3400e8f7be4fd
Pasi Lammi <pasi.lammi@innofactor.com>
2020-06-12 17:53:53 +0300

    update

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
+# 20200612
 FROM alpine:latest
 MAINTAINER pasi@pashi.net
-
 RUN apk add --update openssh
-ADD start.sh /
-
-EXPOSE 22
-ENTRYPOINT [ "/bin/sh", "/start.sh" ]
+ADD proxy_ssh_config /etc/ssh/sshd_config
+ADD start.sh /start.sh
+ADD keys.txt /keys.txt
+EXPOSE 12022
+CMD /start.sh
--- start.sh
+++ start.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
-/usr/bin/ssh-keygen -A
-adduser -S -u 5005 tunnel
-mkdir -p /home/tunnel/.ssh
-if [ -f /key ]
-then
-cp /key /home/tunnel/.ssh/authorized_keys
-fi
-
-chown -R tunnel /home/tunnel/
-chmod 700 /home/tunnel/.ssh
-chmod 600 /home/tunnel/.ssh/authorized_keys
-
-PASS=$(echo -n "tunnel:" ; < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo)
-echo "${PASS}"
-echo "${PASS}" | chpasswd
-
-exec /usr/sbin/sshd -D
+adduser -h /home/proxy -S -D -s /bin/ash proxy
+P=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+echo $P
+echo "proxy:${P}"|chpasswd -c sha256
+mkdir -p /home/proxy/etc/ssh/ /home/proxy/.ssh
+ssh-keygen -A -f /home/proxy
+cat /keys.txt > /home/proxy/.ssh/authorized_keys
+chmod 600 /home/proxy/.ssh/authorized_keys
+chown -R proxy:nobody /home/proxy
+/usr/sbin/sshd -D -f /etc/ssh/sshd_config
